8728e314f423142aa4132a1496b9367d517faf99
divyaum11 <divyamane474@gmail.com>
2024-05-09 17:33:39 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel 11 Multi Auth</title>
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    </head>
+    <body class="bg-light">
+        <section class=" p-3 p-md-4 p-xl-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5">
+                        <div class="card border border-light-subtle rounded-4">
+                            <div class="card-body p-3 p-md-4 p-xl-5">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="mb-1">
+                                            <h4 class="text-center">Welcome</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <hr class="mt-5 mb-4 border-secondary-subtle">
+                                        <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-center">
+                                            <a href="{{ route('account.login') }}" class="link-secondary text-decoration-none">Click here to login</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(["prefix" => "account"], function () {
+    Route::group(["middleware" => "guest"], function () {
+        Route::get('/login', [LoginController::class, 'index'])->name('account.login');
+        Route::get('/register', [LoginController::class, 'register'])->name('account.register');
+        Route::post('/process-register', [LoginController::class, 'processRegister'])->name('account.processRegister');
+        Route::post('/authenticate', [LoginController::class, 'authenticate'])->name('account.authenticate');
+    });
+
+    Route::group(["middleware" => "auth"], function () {
+        Route::get('/logout', [LoginController::class, 'logout'])->name('account.logout');
+        Route::get('/user/dashboard', [DashboardController::class, 'dashboard'])->name('account.dashboard');
+    });
+});
+Route::group(["prefix" => "admin"], function () {
+    Route::group(["middleware" => "auth"], function () {
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    });
+});
